ae73a76ee9f2740045f9c27e625f33aaaa2366c4
Ilya Nemtsov <nemtsovilya@yahoo.com>
2017-01-11 21:27:25 -0800

    Update app.js

--- weather app/app.js
+++ weather app/app.js
@@ -1,1 +1,29 @@
+var weather = require('./weather.js');
+var location = require('./location.js');
+var argv = require('yargs')
+	.option('location', {
+		alias: 'l',
+		demand: false,
+		describe: 'Location to fetch weather for',
+		type: 'string'
+	})
+	.help('help')
+	.argv;
 
+if( typeof argv.l ==='string' && argv.l.length > 0){
+	console.log('Locations was provided');
+	weather(argv.l).then(function(currentweather){
+		console.log(currentweather);
+	}).catch(function(error){
+		console.log(error);
+	})
+}else{
+	console.log('Locations was not provided');
+	location().then(function(loc){
+		return weather(loc.city);
+	}).then(function(currentweather){
+		console.log(currentweather);
+	}).catch(function(error){
+		console.log(error);
+	});
+}
