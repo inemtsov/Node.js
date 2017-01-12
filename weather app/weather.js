6bd7366efb67150fddcaaebee1b9d363b1f8bff7
Ilya Nemtsov <nemtsovilya@yahoo.com>
2017-01-11 21:29:37 -0800

    Update weather.js

--- weather app/weather.js
+++ weather app/weather.js
@@ -1,1 +1,22 @@
+var request = require('request');
 
+module.exports = function (location) {
+		return new Promise(function(resolve, reject){
+		var encodedLocation = encodeURIComponent(location);	
+		var url = 'http://api.openweathermap.org/data/2.5/weather?appid=c11ef074ea7dde400f4a7ea38167c6d8&q=' + encodedLocation + '&units=imperial';
+
+		if (!location){
+			return reject('No location provided!');
+		}
+		request({
+			url: url,
+			json: true
+		}, function(error, response, body){
+			if (error){
+				reject('Unable to fetch the weather!');
+			} else{
+				resolve('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+			}
+		});
+	})
+};
